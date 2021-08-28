031b674929898fabed2dc8a3213a8b79609f3514
shakilkhanSAU <shakilkhan.sau@gmail.com>
2021-08-28 15:38:23 +0600

    formattingd

--- js/random-user.js
+++ js/random-user.js
@@ -5,6 +5,7 @@ const loadUsers = () => {
 };
 
 const displayUser = (user) => {
+    console.log(user)
     const container = document.getElementById('user')
     const div = document.createElement('div');
     container.textContent = '';
@@ -14,19 +15,20 @@ const displayUser = (user) => {
             <img class="rounded-circle" src="${user.picture.large}" alt="">
         </div>
         <div class="card-body">
-        <h4 class="text-center">${user.name.title} ${user.name.first} ${user.name.last} </h4>
-            <p class="card-text">Hey, I'm ${user.name.title} ${user.name.first} ${user.name.last}. I live in ${user.location.country}.</p>
-        <div>
-            <p id="info">Click button for more</p>
-            <p id="email" class="card-text">Email address: <span class="fw-bold"> ${user.email}</span>
+            <h4 class="text-center">${user.name.title} ${user.name.first} ${user.name.last} </h4>
+            <p class="card-text">Hey, my name is ${user.name.title} ${user.name.first} ${user.name.last}. I'm <span class="fw-bold">${user.dob.age}</span> years old.
             </p>
-            <p id="phone" class="card-text">Phone number: <span class="fw-bold"> ${user.phone}</span>
-            </p>
-            <p id="address" class="card-text mb-3">
-            city: <span class="fw-bold"> ${user.location.city}</span>, State: <span class="fw-bold">
-            ${user.location.state}</span> and postcode: <span class="fw-bold"> ${user.location.postcode}</span>
-            </p>
-        </div>
+            <div>
+                <p id="info">Click button for more</p>
+                <p id="email" class="card-text">Email address: <span class="fw-bold"> ${user.email}</span>
+                </p>
+                <p id="phone" class="card-text">Phone number: <span class="fw-bold"> ${user.phone}</span>
+                </p>
+                <p id="address" class="card-text mb-3">
+                city: <span class="fw-bold"> ${user.location.city}</span>, State: <span class="fw-bold">
+                ${user.location.state}</span> and postcode: <span class="fw-bold"> ${user.location.postcode}</span>
+                </p>
+            </div>
             <button onclick="displayEmail('email', 'phone', 'address')" type="button" class="btn btn-light">Email</button>
             <button onclick="displayEmail('phone', 'email', 'address')" type="button" class="btn btn-light">Phone Number</button>
             <button onclick="displayEmail('address', 'email', 'phone')" type="button" class="btn btn-light">Address</button>
